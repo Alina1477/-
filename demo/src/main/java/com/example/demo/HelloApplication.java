ec571007d1068056d93f060333c4f3b0eaf4dfec
easho <eashotov@inbox.ru>
2023-12-11 21:53:50 +1000

    салам

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +17,20 @@ public class HelloApplication extends Application {
         stage.setTitle("KZKZKZKZ?");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public class SQLiteConnection {
+        private static final String JDBC_URL = "jdbc:sqlite:db.db";
+
+        public static Connection connect() {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                return DriverManager.getConnection(JDBC_URL);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to connect to the database");
+            }
+        }
     }
 
     public static void main(String[] args) {
